b4d85c5cbf26b29a1b3f8ecddf865236161aa6e9
Nathan Totten <ntotten@microsoft.com>
2012-06-18 13:16:02 -0300

    Initial Commit

---
+++ src/MessageMe/MessageScott/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MessageScott.Models;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace MessageScott.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMessage(string msg)
+        {
+            // Create Queue Client
+            var queueClient = QueueClient.Create("scott");
+
+            // Create and Send Message to Queue
+            var brokeredMessage = new BrokeredMessage(msg);
+            queueClient.Send(brokeredMessage);
+
+            // Redisplay home-page
+            return RedirectToAction("Index");
+        }
+
+    }
+}
